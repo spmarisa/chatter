3fdfc4f8370bb575d968387b09154653e6c1756d
spmarisa <phaneendra.marisa@gmail.com>
2021-05-27 23:16:15 +0530

    Added a chat_room channel.

--- lib/chatter_web/channels/user_socket.ex
+++ lib/chatter_web/channels/user_socket.ex
@@ -3,6 +3,7 @@ defmodule ChatterWeb.UserSocket do
 
   ## Channels
   # channel "room:*", ChatterWeb.RoomChannel
+  channel "chat_room:lobby", ChatterWeb.ChatRoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
